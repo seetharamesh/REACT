81772175a95d2c8ccc3cfe5d78ae90ce81f34402
seetharamesh <seetharamesh@hotmail.com>
2021-01-14 09:45:51 -0500

    moved props from parent to all components the right way

--- Labs/tardis/DivThree.js
+++ Labs/tardis/DivThree.js
@@ -5,7 +5,7 @@ class DivThree extends React.Component {
       console.log(this.props.passingProp1);
         return (
           <div id="divthree">
-              <h3 onClick={() => this.changeIt(this.props.passingProp1)}> {this.props.passingProp2}</h3>
+              <h3 onClick={() => this.props.passingProp2(this.props.passingProp1)}> {this.props.passingProp1}</h3>
           </div>
         )
     }
