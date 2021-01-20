55ca9d0e862640d5d70128319288e9ab759b26bb
seetharamesh <seetharamesh@hotmail.com>
2021-01-20 18:08:20 -0500

    react mastermind

---
+++ react-mastermind/src/pages/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const SettingsPage = () => {
+    return (
+        <>
+            <Link to='/'> HOME </Link>
+            <h1>Settings Page</h1>
+        </>
+    )
+}
+
+export default SettingsPage;
